f95d9debc4165cc29ea1f6cdc331dceea50c9255
peterbillhu <peterbill26@hotmail.com>
2021-12-03 11:52:57 +0800

    Delete Gray_to_Binary_T.m

--- Gray_to_Binary_T.m
+++
@@ -1,9 +0,0 @@
-function output = Gray_to_Binary_T(input, T)
-    meanValue = mean(mean(input));
-    index_1 = find(input > T);
-    index_0 = find(input <= T);
-    output = input;
-    output(index_1) = 1;
-    output(index_0) = 0;
-end
-
