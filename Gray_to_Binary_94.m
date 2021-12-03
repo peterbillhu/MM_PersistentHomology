75b03325b9119bfcf2cd741ace304b59d5841604
peterbillhu <peterbill26@hotmail.com>
2021-12-03 11:52:38 +0800

    Delete Gray_to_Binary_94.m

--- Gray_to_Binary_94.m
+++
@@ -1,9 +0,0 @@
-function output = Gray_to_Binary_94(input)
-    meanValue = mean(mean(input));
-    index_1 = find(input > 94);
-    index_0 = find(input <= 94);
-    output = input;
-    output(index_1) = 1;
-    output(index_0) = 0;
-end
-
